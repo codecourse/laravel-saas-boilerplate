5674ac802da4691e90d152622f147132366abdc0
Alex Garrett-Smith <alex@codecourse.com>
2023-11-27 18:18:10 +0000

    All code

---
+++ app/Models/TwoFactor.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Model;
+
+class TwoFactor extends Model
+{
+    protected $table = 'two_factor';
+
+    protected $fillable = [
+        'phone', 'dial_code', 'identifier', 'verified'
+    ];
+
+    public static function boot()
+    {
+        static::creating(function ($twoFactor) {
+            optional($twoFactor->user->twoFactor)->delete();
+        });
+    }
+
+    public function isVerified()
+    {
+        return $this->verified;
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
